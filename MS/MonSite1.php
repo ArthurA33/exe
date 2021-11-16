bab7b5e69be287346b880a638beee111566575e0
ArthurA33 <94461146+ArthurA33@users.noreply.github.com>
2021-11-16 15:05:40 +0100

    Add files via upload

---
+++ MS/MonSite1.php
@@ -0,0 +1,31 @@
+<php?
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="icon" href="logo_agence.png" type="image">
+    <link rel="stylesheet" type="text/css" href="fonction/MonSite1_CSS.css">
+</head>
+<body>
+    <header class="color-hint">
+        <img src="fonction/image/logo_agence.png" alt="ya rien" style="width: 100px; display: flex; justify-content: space-between; align-items: center;">
+        <ul>
+            <li><a href="#home">Acceuil</a></li>
+            <li><a href="#news">Service</a></li>
+            <li><a href="#contact">A propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
+    <main>
+        <div id="rel1" class="relative" style="padding: 30px; text-align: center;">
+            <h1 class="neonText" style="text-transform: uppercase;">cour de programmation HTML, CSS, PHP et MYSQL</h1>
+            <h4 class="neonText2">crée par Arthur Annarumma</h4></div>
+    </main>
+</body>
+</html>
